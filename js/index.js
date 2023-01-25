ecfcc40d7c1bb5589c40c2fc8cc8fe43ac9632fc
Nana Chuto <mail@nanachuto.com>
2023-01-25 09:28:37 +0100

    drew and positioned owner on game area

--- js/index.js
+++ js/index.js
@@ -7,17 +7,53 @@ canvas.height = 576;
 ctx.fillStyle = "white";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-const image = new Image();
-image.src = "./images/background.png";
+const backgroundImage = new Image();
+backgroundImage.src = "./images/background.png";
 
 const playerImage = new Image();
 playerImage.src = "./images/dog_awake.png";
 
-console.log(image);
-image.onload = () => {
-  ctx.scale(canvas.width / image.width, canvas.height / image.height);
-  ctx.drawImage(image, 0, 0);
+const ownerImage = new Image();
+ownerImage.src = "./images/owner_asleep.png";
+
+backgroundImage.onload = () => {
+  ctx.scale(
+    canvas.width / backgroundImage.width,
+    canvas.height / backgroundImage.height
+  );
+  ctx.drawImage(backgroundImage, 0, 0);
+};
+
+playerImage.onload = () => {
   ctx.save();
   ctx.scale(0.25, 0.25);
   ctx.drawImage(playerImage, 1050, 600);
 };
+
+ownerImage.onload = () => {
+  ctx.save();
+  ctx.scale(2.1, 2.1);
+  ctx.drawImage(ownerImage, 840, 70);
+};
+
+function animate() {
+  window.requestAnimationFrame(animate);
+}
+
+// Keydown
+window.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "w":
+      console.log("pressed w key");
+      break;
+    case "a":
+      console.log("pressed a key");
+      break;
+    case "s":
+      console.log("pressed s key");
+      break;
+    case "d":
+      console.log("pressed d key");
+      break;
+  }
+});
